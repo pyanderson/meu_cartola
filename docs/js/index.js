28461933f858cfc4635c35c3088cb77bdd302eb2
Anderson Lima <anderson.sl93@hotmail.com>
2021-08-10 10:02:17 -0300

    round 15

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const host = window.location.host == '' ? 'meucartola.pyanderson.dev' : window.location.host;
-const actual_round = 14;
+const actual_round = 15;
 const user_data = {'team': {}};
 const positions = ['gol', 'lat', 'zag', 'mei', 'ata', 'tec'];
 
