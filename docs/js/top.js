a9e20c8205489d102eb76e3340fcf55540b544a5
Anderson Lima <anderson.sl93@hotmail.com>
2021-08-17 09:24:42 -0300

    round 16

--- docs/js/top.js
+++ docs/js/top.js
@@ -1,27 +1,32 @@
 "use strict";
 
+function top_n(ls, cmp, n) {
+  return ls.sort(cmp).reverse().slice(0, n);
+}
+
 function load_league_stats() {
   fetch(`https://${host}/data/liga.json`)
     .then(function (response) {
       return response.json();
     })
     .then(function (league) {
+      const top_size = 5;
       const cmp_by_points = function (a, b) {
         return a['pontos'] - b['pontos'];
       };
       const cmp_by_presence = function (a, b) {
         return a['escalado'] - b['escalado'];
       };
-      for (const player of Object.values(league['atletas']).sort(cmp_by_points).reverse().slice(0, 5)){
+      for (const player of top_n(Object.values(league['atletas']), cmp_by_points, top_size)) {
         $('#top-points-table').append(render_top_row(player));
       }
-      for (const player of Object.values(league['atletas']).sort(cmp_by_presence).reverse().slice(0, 5)){
+      for (const player of top_n(Object.values(league['atletas']), cmp_by_presence, top_size)) {
         $('#top-presence-table').append(render_presence_row(player));
       }
-      for (const team of Object.values(league['clubes']).sort(cmp_by_points).reverse().slice(0, 5)){
+      for (const team of top_n(Object.values(league['clubes']), cmp_by_points, top_size)) {
         $('#top-team-points-table').append(render_top_team_row(team));
       }
-      for (const team of Object.values(league['clubes']).sort(cmp_by_presence).reverse().slice(0, 5)){
+      for (const team of top_n(Object.values(league['clubes']), cmp_by_presence, top_size)) {
         $('#top-team-presence-table').append(render_presence_team_row(team));
       }
     })
