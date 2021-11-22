0d2e3a506ccb3c6429f993ec27a7aea6edd6e3e4
Anderson Lima <anderson.sl93@hotmail.com>
2021-11-22 18:06:30 -0300

    round 34

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const host = window.location.host == '' ? 'meucartola.pyanderson.dev' : window.location.host;
-const actual_round = 33;
+const actual_round = 34;
 const user_data = {'team': {}};
 const db = {'rounds': {}, 'best_rounds': {}};
 const state = {};
